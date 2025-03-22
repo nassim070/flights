247b96758b5042b907ee137e983f7c7caed9f499
ChakirHazard17 <chakirr.7@icloud.com>
2025-03-22 17:04:31 +0100

    statistics_adj.part5

--- scripts/statistics.py
+++ scripts/statistics.py
@@ -1,55 +1,35 @@
 import sqlite3
 import pandas as pd
 
-#conn = sqlite3.connect("data/flights_database.db")
-#df = pd.read_sql_query("SELECT * FROM airports;", conn)
+conn = sqlite3.connect("data/flights_database.db")
+df = pd.read_sql_query("SELECT * FROM flights;", conn)
 
-def get_number_of_flights(df, dep, arr, conn):
-    query = "SELECT COUNT(*) FROM flights WHERE origin = ? AND dest = ?;"
-    cursor = conn.cursor()
-    cursor.execute(query, (dep  , arr))
-    return cursor.fetchone()[0]
 
-def get_average_dep_delay(df, dep, arr, conn):
-    query = "SELECT SUM(dep_delay) FROM flights WHERE origin = ? AND dest = ?;"
-    cursor = conn.cursor()
-    cursor.execute(query, (dep  , arr))
-    total_delay = cursor.fetchone()[0]
+def get_number_of_flights(df, dep, arr):
+    return len(df[(df["origin"] == dep) & (df["dest"] == arr)])
     
-    total_flights = get_number_of_flights(df, dep, arr, conn)
 
-    avg_delay = total_delay / total_flights
+def get_average_dep_delay(df, dep, arr):
+    avg_delay = df[(df["origin"] == dep) & (df["dest"] == arr)]["dep_delay"].mean()
     minutes = int(avg_delay)
     seconds = round((avg_delay - minutes) * 60) 
 
     return f"{minutes} minutes and {seconds} seconds"
 
-def get_percentage_delayed_flights(df, dep, arr, conn):
-    query = "SELECT COUNT(*) FROM flights WHERE origin = ? AND dest = ? AND dep_delay > 0;"
-    cursor = conn.cursor()
-    cursor.execute(query, (dep  , arr))
-    delayed_flights = cursor.fetchone()[0]
 
-    total_flights = get_number_of_flights(df, dep, arr, conn)
+def get_percentage_delayed_flights(df, dep, arr):
+    delayed_flights = len(df[(df["origin"] == dep) & (df["dest"] == arr) & (df["dep_delay"] > 0)])
+    total_flights = get_number_of_flights(df, dep, arr)
 
     return f"{round((delayed_flights / total_flights) * 100, 1)}%"
+    
 
-def get_percentage_on_time_arrivals(df, dep, arr, conn):
-    query = "SELECT COUNT(*) FROM flights WHERE origin = ? AND dest = ? AND arr_delay <= 0;"
-    cursor = conn.cursor()
-    cursor.execute(query, (dep  , arr))
-    on_time_flights = cursor.fetchone()[0]
+def get_percentage_on_time_arrivals(df, dep, arr):
+    on_time_arrivals = len(df[(df["origin"] == dep) & (df["dest"] == arr) & (df["arr_delay"] <= 0)])
+    total_flights = get_number_of_flights(df, dep, arr)
 
-    total_flights = get_number_of_flights(df, dep, arr, conn)
+    return f"{round((on_time_arrivals / total_flights) * 100, 1)}%"
+    
 
-    return f"{round((on_time_flights / total_flights) * 100, 1)}%"
-
-def get_distance(df, dep, arr, conn):
-    query = "SELECT distance FROM flights WHERE origin = ? AND dest = ?;"
-    cursor = conn.cursor()
-    cursor.execute(query, (dep  , arr))
-
-    return f"{cursor.fetchone()[0]} miles"
-
-
-
+def get_distance(df, dep, arr):
+    return df[(df["origin"] == dep) & (df["dest"] == arr)]["distance"].mean()
